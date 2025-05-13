85d30de86769d523321d7d0e8d25c681fadf71e6
AymaneElaamaj <aymanee341gmail.com>
2025-05-13 22:44:11 +0200

    navbar finished

--- src/Component/Navigation/Navigation.jsx
+++ src/Component/Navigation/Navigation.jsx
@@ -1,36 +1,71 @@
 import React from "react";
-import { BsCart } from "react-icons/bs"; // Import de l'icône panier
+import { BsCart, BsSearch } from "react-icons/bs";
 
 const Navigation = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light fixed-top">
-            <div className="collapse navbar-collapse" id="navbarNav">
-    {/* Logo à gauche */}
-    <h2 className="navbar-brand me-auto mb-0">myShop</h2>
-    
-    {/* Menu centré */}
-    <ul className="navbar-nav mx-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Shop</a>
+        <nav className="navbar navbar-expand-lg bg-white fixed-top shadow-sm py-3">
+            <div className="container-fluid px-4">
+                {/* Logo à gauche */}
+                <a className="navbar-brand me-5" href="#">
+                    <h2 className="fw-bold text-primary mb-0">myShop</h2>
+                </a>
+
+                {/* Bouton toggle mobile */}
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarContent"
+                    aria-controls="navbarContent"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* Contenu principal */}
+                <div className="collapse navbar-collapse" id="navbarContent">
+                    {/* Barre de recherche centrée */}
+                    <div className="d-flex align-items-center mx-4 flex-grow-1">
+                        <div className="input-group">
+                            <input 
+                                type="text" 
+                                className="form-control border-end-0" 
+                                placeholder="Rechercher des produits..." 
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-secondary border-start-0" type="button">
+                                <BsSearch />
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Menu navigation */}
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li className="nav-item mx-2">
+                            <a className="nav-link fw-medium" href="#">Shop</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">MEN</a>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link fw-medium" href="#">MEN</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Women</a>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link fw-medium" href="#">Women</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Kids</a>
+                        <li className="nav-item mx-2">
+                            <a className="nav-link fw-medium" href="#">Kids</a>
                         </li>
                     </ul>
-    
-    {/* Panier à droite */}
-    <div className="ms-auto">
-        <a className="nav-link" href="#">
-            <BsCart size={20} />
-        </a>
-    </div>
-</div>
+
+                    {/* Panier avec badge */}
+                    <div className="d-flex ms-4">
+                        <a className="nav-link position-relative" href="#">
+                            <BsCart size={20} />
+                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill ">
+                                
+                                <span className="visually-hidden">articles dans le panier</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 };
