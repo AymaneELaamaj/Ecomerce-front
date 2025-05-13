623f170fdeaa135351bcf15619b231dd20ac9b25
AymaneElaamaj <aymanee341gmail.com>
2025-05-13 22:32:22 +0200

    ADD Navigation

--- src/Component/Navigation/Navigation.jsx
+++ src/Component/Navigation/Navigation.jsx
@@ -1,24 +1,15 @@
 import React from "react";
+import { BsCart } from "react-icons/bs"; // Import de l'icône panier
 
 const Navigation = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light fixed-top">  
-            <div className="container-fluid">
-                <h2 className="navbar-brand mb-0">myShop</h2>
-                
-                {/* Bouton pour mobile */}
-                <button 
-                    className="navbar-toggler" 
-                    type="button" 
-                    data-bs-toggle="collapse" 
-                    data-bs-target="#navbarNav"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                
-                {/* Menu principal */}
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav mx-auto">  {/* ms-auto pour aligner à droite */}
+        <nav className="navbar navbar-expand-lg bg-light fixed-top">
+            <div className="collapse navbar-collapse" id="navbarNav">
+    {/* Logo à gauche */}
+    <h2 className="navbar-brand me-auto mb-0">myShop</h2>
+    
+    {/* Menu centré */}
+    <ul className="navbar-nav mx-auto">
                         <li className="nav-item">
                             <a className="nav-link" href="#">Shop</a>
                         </li>
@@ -31,15 +22,15 @@ const Navigation = () => {
                         <li className="nav-item">
                             <a className="nav-link" href="#">Kids</a>
                         </li>
-                        
                     </ul>
-                    <li className="nav-item ms-auto">
-                             <a className="nav-link" href="#">
-                               <i className="bi bi-cart"></i> Panier
-                            </a>
-                    </li>
-                </div>
-            </div>
+    
+    {/* Panier à droite */}
+    <div className="ms-auto">
+        <a className="nav-link" href="#">
+            <BsCart size={20} />
+        </a>
+    </div>
+</div>
         </nav>
     );
 };
